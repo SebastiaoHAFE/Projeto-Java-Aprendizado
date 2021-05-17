a08eb4f61ba90feb6d2cda9e3a1cca7b7647e232
tiaoh <tiaoh@DESKTOP-EK5VSO0>
2021-05-17 20:27:55 -0300

    Projeto de aprendizado no java

---
+++ projeto-java-treinamento/src/TreinamentoFocoDeterminacao/exercicios.java
@@ -0,0 +1,22 @@
+package TreinamentoFocoDeterminacao;
+
+public class exercicios {
+
+	public static void main(String[] args) {
+		System.out.println("Ola Mundo");
+		System.out.println("Pra Quem Tem Fé, A Vida Não Tem Fim.");
+		
+		System.out.println("----------------------------------------");
+		
+		String nome = "Sebastião";
+		String idade = "35";
+		String objetivo = "Ser Programador Java Full Stack";
+		
+		System.out.println("Prazer meu nome é: " + nome);
+		System.out.println("Tenho: " + idade);
+		System.out.println("Meu Objetivo: " + objetivo);
+		
+		System.out.println("------------------------------------------------");
+		
+	}
+}
