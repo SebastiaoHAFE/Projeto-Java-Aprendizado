28f1504ceaf05dd7ed644b9f8ea2ae10451ac074
tiaoh <tiaoh@DESKTOP-EK5VSO0>
2021-05-17 21:54:16 -0300

    Add a Função double

--- projeto-java-treinamento/src/TreinamentoFocoDeterminacao/exercicios.java
+++ projeto-java-treinamento/src/TreinamentoFocoDeterminacao/exercicios.java
@@ -5,19 +5,30 @@ public class exercicios {
 	public static void main(String[] args) {
 		System.out.println("Ola Mundo");
 		System.out.println("Pra Quem Tem Fé, A Vida Não Tem Fim.");
-		
+
 		System.out.println("----------------------------------------");
-		
+
 		String nome = "Sebastião";
 		String idade = "35";
 		String objetivo = "Ser Programador Java Full Stack";
-		
+
 		System.out.println("Prazer meu nome é: " + nome);
 		System.out.println("Tenho: " + idade);
 		System.out.println("Meu Objetivo: " + objetivo);
-		
+
 		System.out.println("------------------------------------------------");
-	
-		
+
+		String Nome = "Sebastião";
+
+		double nota1 = 70;
+		double nota2 = 80;
+		double nota3 = 60;
+		double nota4 = 70;
+
+		double media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+		System.out.println("A Nota do Aluno " + Nome);
+		System.out.println("A Media do Aluno é " + media);
+
 	}
 }
